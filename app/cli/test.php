3017370969d35e69b2f8dc1dd4ce379d322decec
PhperSid <PhperSid@163.com>
2016-05-20 17:32:14 +0800

    add test script

--- app/cli/test.php
+++ app/cli/test.php
@@ -1,8 +1,36 @@
 <?php defined("__FRAMEWORKNAME__") or die("No permission to access!");
 /**
+ * 写一条消息到队列中 (**不可用循环来代替一次性多条消息入列**)
  * @pageroute
- * 用户列表
  */
-function index(){
-    dump($GLOBALS['cli_args']);
+function mcQueue(){
+    $mcQueue = new \Lib\McQueue();
+
+    $data =  ['user_id' => 777 ,'realname'=> '刘奇','mobile'=>'18811176547','ip' => '127.0.0.1','datetime' => date('Y-m-d H:i:s')];
+    $putStatus = $mcQueue->put('register',$data);
+
+    if(!$putStatus)
+    {
+        $error = $mcQueue->getErrMsg();//  ['err_code' => $mcQueue->errCode ,'err_msg' => $mcQueue->errMsg];
+        dump($error);
+    }
+
+}
+
+/**
+ * 写多条消息到队列中
+ * @pageroute
+ */
+function mcQueueMulti(){
+    $mcQueue = new \Lib\McQueue();
+
+    $item = ['user_id' => 777 ,'realname'=> '刘奇','mobile'=>'18811176547','ip' => '127.0.0.1','datetime' => date('Y-m-d H:i:s')];
+    $datas = array_fill(0, 10000, $item);
+    $putStatus = $mcQueue->put('register',$datas);
+    if(!$putStatus)
+    {
+        $error = $mcQueue->getErrMsg();//  ['err_code' => $mcQueue->errCode ,'err_msg' => $mcQueue->errMsg];
+        dump($error);
+    }
+
 }
